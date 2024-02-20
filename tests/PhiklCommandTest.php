90c927992d3d2e0cb6d8ec41f1c28000522b6536
Alexandre Daubois <2144837+alexandre-daubois@users.noreply.github.com>
2024-02-20 20:18:34 +0100

    Add some functional tests

---
+++ tests/PhiklCommandTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Phpkl\Tests;
+
+use Phpkl\Internal\Command\Runner;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Process\Process;
+
+#[CoversClass(Runner::class)]
+class PhiklCommandTest extends TestCase
+{
+    public static function setUpBeforeClass(): void
+    {
+        $_ENV['PKL_CLI_BIN'] = __DIR__.'/../vendor/bin/pkl';
+    }
+
+    public function testVersion(): void
+    {
+        $process = new Process(['php', __DIR__.'/../phikl', 'version']);
+        $process->mustRun();
+
+        $this->assertMatchesRegularExpression('/(.*)Running Pkl \d+.\d+.\d+ \(.+\)/', $process->getOutput());
+    }
+
+    public function testInstallAlreadyPresent(): void
+    {
+        $process = new Process(['php', __DIR__.'/../phikl', 'install']);
+        $process->mustRun();
+
+        $this->assertMatchesRegularExpression('/(.*)Pkl CLI is already installed in (.+)/', $process->getOutput());
+    }
+
+    public function testCanEval(): void
+    {
+        $process = new Process(['php', __DIR__.'/../phikl', 'eval', 'tests/Fixtures/user.pkl']);
+        $process->mustRun();
+
+        $this->assertSame(<<<PKL
+user {
+  id = 1
+  name = "John Doe"
+  address {
+    street = "123 Main St"
+    city = "Springfield"
+    state = "IL"
+    zip = "62701"
+  }
+}
+PKL, trim($process->getOutput()));
+    }
+}
