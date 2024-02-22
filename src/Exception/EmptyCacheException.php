f246393f9972a85c1061147f895383cabf9078e4
Alexandre Daubois <2144837+alexandre-daubois@users.noreply.github.com>
2024-02-22 15:08:20 +0100

    Add licence headers

--- src/Exception/EmptyCacheException.php
+++ src/Exception/EmptyCacheException.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * (c) Alexandre Daubois <alex.daubois@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Phikl\Exception;
 
 class EmptyCacheException extends \RuntimeException
--- src/Internal/Command/Runner.php
+++ src/Internal/Command/Runner.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * (c) Alexandre Daubois <alex.daubois@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Phikl\Internal\Command;
 
 use Phikl\Cache\PersistentCache;
